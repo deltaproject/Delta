5d4878c5a2fff9701a4dbe0c70f23c97b905e445
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2018-11-16 16:32:06 +0100

    Added sendNotify method for sending notifications

--- js/index.js
+++ js/index.js
@@ -352,6 +352,27 @@ function refreshData(homeworkOnly = false) {
     });
 }
 
+function sendNotify(caption, notificationType) {
+    if (notificationType == undefined
+        || notificationType == null) {
+        notificationType = "";
+    }
+
+    var container = document.getElementById("notifyContainer");
+    var notify = document.createElement("div");
+    notify.className = `notification ${notificationType}`;
+    notify.innerText = caption;
+
+    container.appendChild(notify);
+
+    setTimeout(() => {
+        notify.className += " dismissed";
+        setTimeout(() => {
+            container.removeChild(notify);
+        }, 500);
+    }, 3000);
+}
+
 app.profile.username = m.profileInfo().fullName();
 app.agendaDate = dayFormat;
 
