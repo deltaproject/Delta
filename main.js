96597de1c633e6f5614f6cd602e2249d1615fcde
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2018-11-17 21:02:07 +0100

    Finally fixed authentication 🎉

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const _ = require("lodash");
 const Magister = require("magister.js");
 const electron = require("electron");
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("path");
 const url = require("url");
 const isDev = require('electron-is-dev');
@@ -12,47 +12,57 @@ if (isDev) {
     });
 }
 
-function createWindow() {
-    // Menu.setApplicationMenu(null);
+let mainWin;
+let authWin;
 
-    mainWin = new BrowserWindow({ width: 1220, height: 800,
+function createWindow() {
+    mainWin = new BrowserWindow({
+        width: 1220,
+        height: 800,
+        show: false,
         icon: path.join(__dirname, 'img/icons/icon.png')
     });
 
-    // authWin = new BrowserWindow({ width: 1220, height: 800,
-    //     icon: path.join(__dirname, 'img/icons/icon.png')
-    // });
-
-    mainWin.loadURL(url.format({
-        pathname: path.join(__dirname, "index.html"),
-        protocol: "file:",
-        slashes: true
-    }));
-
-    // authWin.loadURL(url.format({
-    //     pathname: path.join(__dirname, "auth.html"),
-    //     protocol: "file:",
-    //     slashes: true
-    // }));
+    authWin = new BrowserWindow({
+        parent: mainWin,
+        width: 1220,
+        height: 800,
+        icon: path.join(__dirname, 'img/icons/icon.png')
+    });
 
     mainWin.on("closed", function () {
         mainWin = null;
-        // authWin = null;
+        authWin = null;
     });
+
+    authWin.loadURL(url.format({
+        pathname: path.join(__dirname, "auth.html"),
+        protocol: "file:",
+        slashes: true
+    }));
 }
 
-var m = new Magister.Magister({
-    school: '',
-    username: '',
-    password: ''
+ipcMain.on("login-success", (event, creds) => {
+    var m = new Magister.Magister({
+        school: creds.school,
+        username: creds.username,
+        password: creds.password
+    });
+
+    m.ready(function () {
+        global.m = m;
+
+        mainWin.loadURL(url.format({
+            pathname: path.join(__dirname, "index.html"),
+            protocol: "file:",
+            slashes: true
+        }));
+
+        authWin.close();
+        mainWin.show();
+    })
 });
 
 app.on("ready", function () {
-    m.ready(function () {
-        console.log("Magister is ready.");
-        global.m = m;
-        global.magister = Magister;
-
-        createWindow();
-    });
+    createWindow();
 });
