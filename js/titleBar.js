3b17b2ccbb79a43d79e8b32a82981446d66452ba
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2018-11-30 12:42:49 +0100

    Added titleBar.js & Vue component

---
+++ js/titleBar.js
@@ -0,0 +1,13 @@
+Vue.component('title-bar', {
+    template: `
+    <div class="titleBar no-select">
+        <div id="closeBtn">
+            <i class="fas fa-times"></i>
+        </div>
+            
+        <div id="minimizeBtn">
+            <i class="fas fa-window-minimize"></i>
+        </div>
+    </div>
+    `
+});
