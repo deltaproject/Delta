47aa956f99905a0ade8856aec4a837a7306de627
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2018-11-30 18:10:52 +0100

    Dynamic titleBar title

--- js/titleBar.js
+++ js/titleBar.js
@@ -1,3 +1,5 @@
+var $ = require("jquery/dist/jquery");
+
 function minimizeWindow() {
     remote.BrowserWindow.getFocusedWindow().minimize();
 }
@@ -13,12 +15,18 @@ function closeWindow(quitOnClose = true) {
 Vue.component('title-bar', {
     template: `
     <div class="titleBar no-select">
-        <div id="closeBtn" onclick="closeWindow()">
-            <i class="fas fa-times"></i>
+        <div class="segmentLeft">
+            <p>${$("title").html()}</p>
         </div>
+
+        <div class="segmentRight">
+            <div id="closeBtn" onclick="closeWindow()">
+                <i class="fas fa-times"></i>
+            </div>
             
-        <div id="minimizeBtn" onclick="minimizeWindow()">
-            <i class="fas fa-window-minimize"></i>
+            <div id="minimizeBtn" onclick="minimizeWindow()">
+                <i class="fas fa-window-minimize"></i>
+            </div>
         </div>
     </div>
     `
