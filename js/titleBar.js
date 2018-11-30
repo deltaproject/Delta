43b0f4132f2ffeddc8e8b770f11c690f64953718
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2018-11-30 13:26:31 +0100

    Added general minimize & close functionality for titleBar

--- js/titleBar.js
+++ js/titleBar.js
@@ -1,11 +1,23 @@
+function minimizeWindow() {
+    remote.BrowserWindow.getFocusedWindow().minimize();
+}
+
+function closeWindow(quitOnClose = true) {
+    if (quitOnClose) {
+        electron.quit();
+    } else {
+        remote.BrowserWindow.getFocusedWindow().close();
+    }
+}
+
 Vue.component('title-bar', {
     template: `
     <div class="titleBar no-select">
-        <div id="closeBtn">
+        <div id="closeBtn" onclick="closeWindow()">
             <i class="fas fa-times"></i>
         </div>
             
-        <div id="minimizeBtn">
+        <div id="minimizeBtn" onclick="minimizeWindow()">
             <i class="fas fa-window-minimize"></i>
         </div>
     </div>
