1f0f81639bda3e871d3a52f1c47a1c7ce1c10b80
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2018-12-13 21:03:46 +0100

    Implemented Vue app

--- js/app/info.js
+++ js/app/info.js
@@ -1,16 +1,25 @@
 var app = new Vue({
     el: "#app",
     data: {
-        schoolQuery: [],
-        saveCreds: true,
-        loginIncorrect: false,
-        schoolIncorrect: false,
-        isBusy: false,
-        loginSuccess: false,
-        creds: {
-            school: '',
-            username: '',
-            password: ''
-        }
+        header: "bedrijfseconomie",
+        content: null,
+        tableData: [
+            {
+                "name": "Datum",
+                "value": "12 november 2018"
+            },
+            {
+                "name": "Locatie",
+                "value": "102"
+            },
+            {
+                "name": "Docent",
+                "value": "A.B. Test"
+            },
+            {
+                "name": "Opgegeven op",
+                "value": "7 november 2018"
+            }
+        ]
     }
 });
