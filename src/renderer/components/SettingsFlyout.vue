0115e5e1103cf1de39b9236b8fa4aceaef543b9e
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2019-02-03 17:33:40 +0100

    Added SettingsFlyout.vue

---
+++ src/renderer/components/SettingsFlyout.vue
@@ -0,0 +1,22 @@
+<template>
+    <transition name="settings">
+      <div class="settingsMenu no-select" v-show="isSettingsMenu">
+          <div class="closeSettings" @click="toggleSettings()">
+              <i class="fas fa-times"></i>
+          </div>
+
+          <h1 class="username-big">{{ profile.username }}</h1>
+          <h2 class="userDesc">{{ profile.userDesc }}</h2>
+          <p class="btnSignOff" title="Meld je af bij Delta" @click="signOff()">Afmelden</p>
+      </div>
+    </transition>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
