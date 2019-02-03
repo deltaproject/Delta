2a6dba1c200e541c0aecce943e61090ade2129e8
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2019-02-03 17:33:55 +0100

    Implement new components

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,18 +1,26 @@
 <template>
   <div id="app">
     <login-section></login-section>
+    <status-bar></status-bar>
+    <settings-flyout></settings-flyout>
+
+    <div id="notifyContainer"></div>
   </div>
 </template>
 
 <script>
   import LoginSection from '@/components/LoginSection'
+  import StatusBar from '@/components/StatusBar'
+  import SettingsFlyout from '@/components/SettingsFlyout'
 
   const { getSchools } = require('magister.js')
 
   export default {
     name: 'delta',
     components: {
-      LoginSection
+      LoginSection,
+      StatusBar,
+      SettingsFlyout
     },
     data: function () {
       return {
