8fc19708d707732fcbb1a2c59220462970498ee5
Julian van Doorn <jvdoorn@antarc.com>
2019-02-03 15:18:50 +0100

    Added login component as a demonstration.

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,20 +1,56 @@
 <template>
   <div id="app">
-    <landing-page></landing-page>
+    <login-section></login-section>
   </div>
 </template>
 
 <script>
-  import LandingPage from '@/components/LandingPage'
+  import LoginSection from '@/components/LoginSection'
+
+  const { getSchools } = require('magister.js')
 
   export default {
     name: 'delta',
     components: {
-      LandingPage
+      LoginSection
+    },
+    data: function () {
+      return {
+        authentication: {
+          credentials: {
+            schoolname: '',
+            username: '',
+            password: '',
+            token: ''
+          },
+          isGuest: false,
+          saveCredentials: true,
+          schoolQuery: []
+        },
+        state: {
+          authenticated: false,
+          authenticating: false,
+          errors: {
+            invalidSchoolname: false,
+            invalidCredentials: false
+          }
+        }
+      }
+    },
+    methods: {
+      getSchools () {
+        if (this.authentication.credentials.schoolname.length > 2) {
+          getSchools(this.authentication.credentials.schoolname).then((schools) => {
+            this.authentication.schoolQuery = schools
+          })
+        }
+      },
+      login () {
+        console.log('Logging in')
+      }
     }
   }
 </script>
 
 <style>
-  /* CSS */
 </style>
