c5601f8c3ea571e4965d8b294b1c9e5f5336ad74
Julian van Doorn <jvdoorn@antarc.com>
2019-02-03 20:37:31 +0100

    Moved all login variables to LoginSection.vue

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
-    <login-section></login-section>
+    <login-section ref="login-section"></login-section>
   </div>
 </template>
 
 <script>
   import LoginSection from '@/components/LoginSection'
 
-  const { getSchools } = require('magister.js')
+  const { remote } = require('electron')
+  const app = remote.app
+  const path = require('path')
+
+  const schoolFile = path.join(app.getPath('userData'), 'school.json')
+  const credentialsFile = path.join(app.getPath('userData'), 'credentials.json')
 
   export default {
     name: 'delta',
@@ -16,38 +21,13 @@
     },
     data: function () {
       return {
-        authentication: {
-          credentials: {
-            schoolname: '',
-            username: '',
-            password: '',
-            token: ''
-          },
-          isGuest: false,
-          saveCredentials: true,
-          schoolQuery: []
-        },
-        state: {
-          authenticated: false,
-          authenticating: false,
-          errors: {
-            invalidSchoolname: false,
-            invalidCredentials: false
-          }
+        files: {
+          schoolFile: schoolFile,
+          credentialsFile: credentialsFile
         }
       }
     },
     methods: {
-      getSchools () {
-        if (this.authentication.credentials.schoolname.length > 2) {
-          getSchools(this.authentication.credentials.schoolname).then((schools) => {
-            this.authentication.schoolQuery = schools
-          })
-        }
-      },
-      login () {
-        console.log('Logging in')
-      }
     }
   }
 </script>
